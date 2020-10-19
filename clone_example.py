4301890b4cd555c0f101c8a784f286d3cd1cc3a7
Richard Gill <richardg@stripe.com>
2020-10-19 13:52:44 +0100

    fixed formatting

--- clone_example.py
+++ clone_example.py
@@ -11,18 +11,18 @@ SECONDARY_STRIPE_ACCOUNT_ID = os.getenv("SECONDARY_STRIPE_ACCOUNT_ID")
 print(PRIMARY_STRIPE_SECRET_KEY)
 
 payment_method = stripe.PaymentMethod.create(
-	type="card",
-	billing_details = {
-		"address": {
-			"postal_code": "E14 5AA"
-		}
-	},
-	card = {
-		"number": "4242424242424242",
-		"cvc": "123",
-		"exp_month": "08",
-		"exp_year": "23"
-	},
+  type="card",
+  billing_details = {
+    "address": {
+      "postal_code": "E14 5AA"
+    }
+  },
+  card = {
+    "number": "4242424242424242",
+    "cvc": "123",
+    "exp_month": "08",
+    "exp_year": "23"
+  },
   api_key=PRIMARY_STRIPE_SECRET_KEY,
 )
 
@@ -31,9 +31,9 @@ input("Press Enter to continue...")
 
 customer = stripe.Customer.create(
   description="Customer 1",
-	metadata={'name': 'customer 1'},
-	payment_method=payment_method.id,
-	api_key=PRIMARY_STRIPE_SECRET_KEY,
+  metadata={'name': 'customer 1'},
+  payment_method=payment_method.id,
+  api_key=PRIMARY_STRIPE_SECRET_KEY,
 )
 
 customer = stripe.Customer.retrieve(customer.id, api_key=PRIMARY_STRIPE_SECRET_KEY)
@@ -44,7 +44,7 @@ input("Press Enter to continue...")
 payment_methods = stripe.PaymentMethod.list(
   customer=customer.id,
   type="card",
-	api_key=PRIMARY_STRIPE_SECRET_KEY,
+  api_key=PRIMARY_STRIPE_SECRET_KEY,
 )
 
 print('primary payment_method: ', payment_methods.data[0])
@@ -52,16 +52,16 @@ input("Press Enter to continue...")
 
 customer_payment_method = payment_methods.data[0]
 payment_intent = stripe.PaymentIntent.create(
-	return_url= "https://something.com",
-	payment_method=customer_payment_method.id,
-	customer=customer.id,
-	amount=1000,
-	currency="GBP",
-	confirm=True,
-	transfer_data={
-		'destination': 'acct_1H5vOFEnc0lv44J2'
-	},
-	api_key=PRIMARY_STRIPE_SECRET_KEY,
+  return_url= "https://something.com",
+  payment_method=customer_payment_method.id,
+  customer=customer.id,
+  amount=1000,
+  currency="GBP",
+  confirm=True,
+  transfer_data={
+    'destination': 'acct_1H5vOFEnc0lv44J2'
+  },
+  api_key=PRIMARY_STRIPE_SECRET_KEY,
 )
 
 
@@ -73,7 +73,7 @@ cloned_payment_method = stripe.PaymentMethod.create(
   customer=customer.id,
   payment_method=customer_payment_method.id,
   stripe_account=SECONDARY_STRIPE_ACCOUNT_ID,
-	api_key=PRIMARY_STRIPE_SECRET_KEY,
+  api_key=PRIMARY_STRIPE_SECRET_KEY,
 )
 
 print('Cloned payment method!: ', cloned_payment_method)
@@ -82,14 +82,14 @@ input("Press Enter to continue...")
 print(cloned_payment_method)
 
 payment_intent = stripe.PaymentIntent.create(
-	return_url= "https://something.com",
-	payment_method=cloned_payment_method.id,
-	amount=1000,
-	currency="GBP",
-	confirm=True,
-	transfer_data={
-		'destination': 'acct_1H5voeLz0gGpGd9R'
-	},
-	api_key=SECONDARY_STRIPE_SECRET_KEY,
+  return_url= "https://something.com",
+  payment_method=cloned_payment_method.id,
+  amount=1000,
+  currency="GBP",
+  confirm=True,
+  transfer_data={
+    'destination': 'acct_1H5voeLz0gGpGd9R'
+  },
+  api_key=SECONDARY_STRIPE_SECRET_KEY,
 )
 
